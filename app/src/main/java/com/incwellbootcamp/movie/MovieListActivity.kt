7d8013cb8c9b0a60d023bbc1eb25bfb9e3d09fbb
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-30 14:37:03 +0545

    setup theme

--- app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
+++ app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
@@ -3,7 +3,7 @@ package com.incwellbootcamp.movie
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
