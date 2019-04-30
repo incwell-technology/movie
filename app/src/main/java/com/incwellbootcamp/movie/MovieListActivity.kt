ec756187cb08248e64301332ff8b5d56fc5519db
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-30 15:11:38 +0545

    add menu item

--- app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
+++ app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
@@ -1,6 +1,9 @@
 package com.incwellbootcamp.movie
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,5 +13,17 @@ class MovieListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.movie_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.search){
+            Toast.makeText(this, "search clicked", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
