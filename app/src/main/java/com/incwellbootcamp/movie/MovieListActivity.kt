49ffa68a69cc8b4a9fc5e335dc21e9775621687a
Rajesh <rajesh.k.khadka@gmail.com>
2019-04-30 14:44:07 +0545

    add toolbar

--- app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
+++ app/src/main/java/com/incwellbootcamp/movie/MovieListActivity.kt
@@ -2,11 +2,13 @@ package com.incwellbootcamp.movie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MovieListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
